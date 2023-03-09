721213de8719aabdb7354014281bc5e74c9fe263
atias6051 <atias6051@gmail.com>
2023-03-09 16:34:53 -0500

    starting routes

---
+++ flask-backend/app/routes/pokemon.py
@@ -0,0 +1,8 @@
+from flask import Flask, Blueprint
+from ..models import Pokemon
+
+bp = Blueprint("pokemon",__name__)
+
+@bp.route('/')
+def get_all_pokemons():
+    return [pokemon.to_dict() for pokemon in Pokemon.query.all()]
