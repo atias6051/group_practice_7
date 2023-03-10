c20ac7721148ad6c5f319f134df4ab21d6915209
Vusal Layijov <Layijovvusal@gmail.com>
2023-03-09 16:04:39 -0800

    Form

--- flask-backend/app/form.py
+++ flask-backend/app/form.py
@@ -1,15 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SelectField, StringField, SubmitField, BooleanField, FloatField, URLField
-from wtforms.validators import DataRequired, URL
+from wtforms.validators import DataRequired, URL, NumberRange
 from .models import PokemonType
-
-selectChoices = list(PokemonType.query.all().type)
-print("###$$$$$$$$$####", selectChoices)
+# selectChoices = PokemonType.query.all()
+# print("###$$$$$$$$$####", selectChoices)
 
 class PokemonForm(FlaskForm):
-    number = IntegerField("Number",validators=[DataRequired()])
-    attack = IntegerField("Attack",validators=[DataRequired()])
-    defence = IntegerField("Defense",validators=[DataRequired()])
+    number = IntegerField("Number", validators=[DataRequired(), NumberRange(min=1, message=None)])
+    attack = IntegerField("Attack", validators=[DataRequired(), NumberRange(min=0, max=100)])
+    defence = IntegerField("Defense", validators=[DataRequired(), NumberRange(min=0, max=100)])
     image_url = StringField("Image URL", validators=[DataRequired()])
     name = StringField("Nmae", validators=[DataRequired()])
-    type = SelectField("Type",choices=selectChoices, validators=[DataRequired()])
+    #type = SelectField("Type",choices=selectChoices, validators=[DataRequired()])
+    type=StringField("Type")
+    move1=StringField("Move1")
+    move2=StringField("Move2")
+    submit=SubmitField("Create New Pokemon")
--- flask-backend/app/routes/pokemon.py
+++ flask-backend/app/routes/pokemon.py
@@ -1,6 +1,6 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint,request
 from ..models import Pokemon
-
+from ..form import PokemonForm
 bp = Blueprint("pokemon", __name__)
 
 @bp.route('/pokemon')
@@ -12,3 +12,13 @@ def get_all_pokemons():
 def get_single_pokemon(id):
     pokemon = Pokemon.query.get(id)
     return pokemon.to_dict()
+
+
+@bp.route('/pokemon', methods=["POST"])
+def create_pokemon():
+    res=request.get_json()
+    form=PokemonForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+    if form.validate_on_submit():
+        pass
+
