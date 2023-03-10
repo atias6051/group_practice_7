a102d5f6cd4bc245b1d15fc95df3758ca17e23bf
NygilNet <nygilnet@gmail.com>
2023-03-09 18:26:15 -0600

    eod

--- flask-backend/app/routes/pokemon.py
+++ flask-backend/app/routes/pokemon.py
@@ -18,7 +18,23 @@ def get_single_pokemon(id):
 def create_pokemon():
     res=request.get_json()
     form=PokemonForm()
+    data = form.data
     form["csrf_token"].data = request.cookies["csrf_token"]
     if form.validate_on_submit():
-        pass
-
+        if data['move2']:
+            move = f"{data['move1']},{data['move2']}"
+        else:
+            move = data['move1']
+        new_pokemon = Pokemon(
+            number=data['number'],
+            attack = data['attack'],
+            defense = data['defense'],
+            img_url = data['img_url'],
+            type = data['type'],
+            moves = move
+        )
+        db.session.add(new_pokemon)
+        db.session.commit()
+        return new_pokemon.to_dict()
+    if form.errors:
+        return { "errors": form.errors }
